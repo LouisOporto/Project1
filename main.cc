cb7fe0825cf4ff72c0a65acef8451aa6cf66665d
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-03-01 12:00:24 -0800

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,22 +1,4 @@
+void player(){
+  
 
-
-#include <iostream>
-
-using namespace std;
-
-int player() {
-  blah blah bloah
-    
-  asdfasd
-  f
-  dsaf
-  asf
-  sdf
-   sd
-    
-    return 0;
 }
-   
-  int main() {
-
-  return 0;
