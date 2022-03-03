59197d6ea50ac5631dd14dee316d7335e30f2585
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-03-02 20:57:12 -0800

    Calling of functions

--- main.cc
+++ main.cc
@@ -1,13 +1,22 @@
-
-
 #include <iostream>
 
-using namespace std;
+#include "cpputils/graphics/image.h"
+//Add the void functions into here for all four void functions //
 
-
-
-}
+int main() {
+   player();
+   opponent();
+   player_projectile();
+   opponent_projectile();
    
-  int main() {
-
-  return 0;
+   std::string player_input, opponent_input, p_projectile, o_projectile;
+   std::cout << "Please provide player image file name: ";
+   std::cin >> player_input;
+   std::cout << "Please provide opponent image filename: ";
+   std::cin >> opponent_input;
+   std::cout << "Please provide player projectile image filename: ";
+   std::cin >> p_projectile;
+   std::cout << "Please provide opponent projectile image filename ";
+   std::cin >> o_projectile;
+   
+return 0;
