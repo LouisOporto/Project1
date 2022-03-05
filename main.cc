4732a66d648d49ac8a8106ab6b53e3df1568cc4b
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-03-05 11:14:40 -0800

    Fixed Inputs and function calling

--- main.cc
+++ main.cc
@@ -5,20 +5,19 @@
 //Add the void functions into here for all four void functions //
 
 int main() {
-   player();
-   opponent();
-   p_projectile();
-   o_projectile();
-   
    std::string player_, opponent_, p_projectile_, o_projectile_;
-   std::cout << "Please provide player image file name: ";
-   std::cin >> player_input_;
+   std::cout << "Please provide player image filename: ";
+   std::cin >> player_;
+   player(player_);
    std::cout << "Please provide opponent image filename: ";
-   std::cin >> opponent_input_;
+   std::cin >> opponent_;
+   opponent(opponent_);
    std::cout << "Please provide player projectile image filename: ";
    std::cin >> p_projectile_;
-   std::cout << "Please provide opponent projectile image filename ";
+   p_projectile(p_projectile_);
+   std::cout << "Please provide opponent projectile image filename: ";
    std::cin >> o_projectile_;
+   o_projectile(o_projectile_);
    
 return 0;
 }
