23d2e5ecce3b85e54b51a758ea6226bd4d64d42c
Flxres <90017757+Flxres@users.noreply.github.com>
2022-03-22 10:03:58 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 void p_projectile(std::string input) {
   graphics::Image p_projectile(5, 5);
- p_projectile.DrawLine( 1, 4, 1, 0, 255,  0, 0, 1);//outside
+  p_projectile.DrawLine( 1, 4, 1, 0, 255,  0, 0, 1);//outside
   p_projectile.DrawLine( 3, 4, 3, 0, 255,  0, 0, 1);//outside
   p_projectile.SaveImageBmp(input);
 }
