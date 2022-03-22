01b9b58ca56906ae80259bb83ff293794b88e7af
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-03-22 09:42:07 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -24,11 +24,33 @@ void player(std::string input){
 //Player Projectile Void
 void p_projectile(std::string input) {
   graphics::Image p_projectile(5, 5);
- p_projectile.DrawLine( 1, 4, 1, 0, 255,  0, 0, 1);//outside
+  p_projectile.DrawLine( 1, 4, 1, 0, 255,  0, 0, 1);//outside
   p_projectile.DrawLine( 3, 4, 3, 0, 255,  0, 0, 1);//outside
   p_projectile.SaveImageBmp(input);
 }
 
+void o_projectile_(std::string input) {
+  graphics::Image oProjectile(10, 10);
+  oProjectile.DrawRectangle(0, 0, 2, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(0, 1, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(1, 2, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(2, 3, 6, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(2, 4, 2, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(3, 5, 1, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(6, 4, 2, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(6, 5, 1, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(6, 2, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(7, 1, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(8, 0, 2, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(2, 6, 6, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(1, 7, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(6, 7, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(0, 8, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(7, 8, 3, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(0, 9, 2, 1, 102, 0, 0);
+  oProjectile.DrawRectangle(8, 9, 2, 1, 102, 0, 0);
+  oProjectile.SaveImageBmp(input);
+}
 int main() {
    std::string player_, opponent_, p_projectile_, o_projectile_;
    std::cout << "Please provide player image filename: ";
