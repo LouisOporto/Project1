24e0c517190c9a2454bf1df93292b8c53779777b
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-03-24 09:09:52 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 void o_projectile(std::string input) {
   graphics::Image oProjectile(5, 5);
-  oProjectile.DrawLine(1, 4, 1, 0, 0,  255, 0, 1);
-  oProjectile.DrawLine
+  oProjectile.DrawLine(1, 4, 1, 0, 0, 255, 0, 1);
+  oProjectile.DrawLine(3, 4, 3, 0, 0, 255, 0, 1);
   oProjectile.SaveImageBmp(input);
 }
