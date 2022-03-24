a96413c6fce4a0e1e0979ce521b9249fc15ba20a
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-03-23 19:49:04 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -110,8 +110,8 @@ void opponent(std::string input) {
 // Opponent Projectile
 void o_projectile(std::string input) {
   graphics::Image oProjectile(5, 5);
-  oProjectile.DrawLine(2, 0, 0, 4, 255,  10, 10, 1);
-  oProjectile.DrawLine(2, 0, 4, 4, 255,  10, 10, 1);
+  oProjectile.DrawLine(2, 0, 0, 4, 255, 10, 10, 1);
+  oProjectile.DrawLine(2, 0, 4, 4, 255, 10, 10, 1);
   oProjectile.DrawLine(2, 1, 1, 4, 102, 0, 0, 1);
   oProjectile.DrawLine(2, 1, 3, 4, 102, 0, 0, 1);
   oProjectile.SaveImageBmp(input);
